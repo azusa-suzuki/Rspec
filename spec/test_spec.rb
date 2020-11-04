6ce871b5889d0944df324ec7b740381077d6838b
EC2 Default User <ec2-user@ip-172-31-8-140.ec2.internal>
2020-11-04 06:23:11 +0000

    Rspec練習用

---
+++ spec/test_spec.rb
@@ -0,0 +1,12 @@
+describe '四則演算' do
+  context 'かけ算' do
+    it '5 * 5 は 25 になる' do
+      expect(5 * 5).to eq 25
+    end
+  end
+  context '足し算' do
+    it '5 * 5 は 25 になる' do
+      expect(5 * 5).to eq 26
+    end
+  end
+end
